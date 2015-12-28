82cd1b20402b1e391a1f1d6c17994f70ed9a50df
shinshin86 <beagles1986@gmail.com>
2015-12-28 13:19:12 +0900

    Change a Directory name

--- postdata.py
+++ postdata.py
@@ -8,7 +8,7 @@ def take_postdata(arg_file):
     # Read a "Connect Info"
     conf = configparser.SafeConfigParser()
     conf.read('config/postdata_dir.cfg')
-    postdatas_path = str(conf.get('postdata','path'))
+    postdatas_path = str(conf.get('postdata','content'))
 
     # os.listdir('パス')
     # 指定したパス内の全てのファイルとディレクトリを要素とするリストを返す
@@ -23,3 +23,20 @@ def take_postdata(arg_file):
                     post_text.append(line)
 
     return post_text
+
+
+
+
+def take_image(arg_file):
+    # Read a "Connect Info"
+    conf = configparser.SafeConfigParser()
+    conf.read('config/postdata_dir.cfg')
+    postdatas_path = str(conf.get('postdata','image'))
+
+    # os.listdir('パス')
+    # 指定したパス内の全てのファイルとディレクトリを要素とするリストを返す
+    files = os.listdir(postdatas_path)
+        for file in files:
+            if file == arg_file:
+
+        return postdatas_path + '/' + file
